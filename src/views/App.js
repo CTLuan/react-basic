a89321f8ee19b1b9b19bf641e1ec29c7aef13170
luan <luan.d18c01a1513@gmail.com>
2023-05-28 16:22:20 +0700

    add

--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='Hello'>
           Hello
         </p>
         <a
